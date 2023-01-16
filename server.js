5d62bd743742b1bdc07838c83171b2e5cfa55d3e
Greg Schoenberg <gregschoenberg@gmail.com>
2023-01-16 13:22:42 -0800

    basics of server.js with express setup for server using ejs

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-// todo - Declare variables 
+const express = require("express");
+const app = express();
+const PORT = 8000;
+const mongoose = require("mongoose");
 
     // *Import functions/routes
 
 // todo - connect to database
 
 
-// todo - set middleware
-
+// set middleware
+// set view engine
+app.set("view engine", "ejs");
+// files will be in public folder, eg css / static html
+app.set(express.static("public"));
+// properly parse requests from forms
+app.use(express.urlencoded({extended: true}));
 
 // todo - set routes
 
 
 // todo - start server
+app.listen(PORT, () => console.log(`server started on port ${PORT}`))
